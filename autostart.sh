97046526d6f961f488d31d6522af65f6d2b95ca5
DioptricDesign <contact@dioptricdesign.com>
2020-09-24 12:43:43 -0600

    Updated autostart for new lock script

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 export DISPLAY=:0
 exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-xautolock -time 5 -locker "i3lock -c 000000" &
+xautolock -time 5 -locker "lock" &
 clipmenud &
 redshift-gtk &
 pamac-tray &
