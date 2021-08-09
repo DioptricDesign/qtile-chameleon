efcbdbc6f33b8d8dcad3ba910fb43097ed4d7e51
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2021-08-09 11:29:11 -0600

    added urxvt daemon, mpd & nm-applet
    removed old qtile restart command.

--- autostart.sh
+++ autostart.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-export DISPLAY=:0
 exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 xautolock -time 5 -locker "lock" &
 clipmenud &
 redshift-gtk &
-pamac-tray &
-walp &
-qtile-cmd -o cmd -f restart 
+nm-applet &
+mpd &
+urxvtd -q -f -o &
+walp & 
