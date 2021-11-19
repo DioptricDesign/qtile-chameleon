83390d22e20f785ca42bf00259d3157ac9f9d7a5
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2021-11-19 11:49:44 -0700

    Update installscript.sh

--- installscript.sh
+++ installscript.sh
@@ -74,7 +74,8 @@ echo Copying Files...
 cp -r "$qtchdl"/qtile-chameleon/* "$qtiledir"
 cp "$qtchdl"/scripts/*.sh "$scriptsdir"
 cp "$qtchdl"/scripts/.Xdefaults "$HOME"
-cp "$qtchdl"/wallpapers/*.png*.jpg "$bgdir"
+cp "$qtchdl"/wallpapers/*.png "$bgdir"
+cp "$qtchdl"/wallpapers/*.jpg "$bgdir"
 sudo cp "$qtchdl"/scripts/bin/* "$bindir"
 sudo cp "$qtchdl"/qtile.desktop "$xdir"
 
